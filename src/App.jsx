5fbb837dbc3f7811819e974cd6a300dbd254fc5a
yokomaru <cabochetoganlan@gmail.com>
2025-07-02 16:45:46 +0900

    add: Create basic CRUD functions using static data
    一旦静的なデータを用いてアプリ全体を作成
    - コンポーネントは以下で設計
    　- MemoApp
            - MemoIndex
               - Memo
            - MemoEditor
               - MemoTextArea
    - ステートは仮置き
         - memos
         - editingMemo
         - text

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,169 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function MemoTextArea({
+  memo,
+  text,
+  setText,
+  memos,
+  handleUpdateButtonClick,
+  handleDeleteButtonClick,
+}) {
+  console.log(memo);
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <textarea
+        name="memoContent"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        rows={4}
+        cols={40}
+      />
+      <button
+        onClick={() => {
+          handleUpdateButtonClick(memo, text);
+        }}
+      >
+        更新
+      </button>
+      <button
+        onClick={() => {
+          handleDeleteButtonClick(memos, memo);
+        }}
+      >
+        削除
+      </button>
+    </div>
+  );
 }
 
-export default App
+function Memo({ memo, handleMemoClick }) {
+  return (
+    <button
+      onClick={() => {
+        handleMemoClick(memo);
+      }}
+    >
+      {memo.title}
+    </button>
+  );
+}
+
+function MemoIndex({ memos, handleMemoClick, handleAddButtonClick }) {
+  return (
+    <section>
+      <ul>
+        {memos.map((memo) => (
+          <li key={memo.id}>
+            <Memo memo={memo} handleMemoClick={handleMemoClick} />
+          </li>
+        ))}
+      </ul>
+      <div>
+        <button
+          onClick={() => {
+            handleAddButtonClick(memos);
+          }}
+        >
+          +
+        </button>
+      </div>
+    </section>
+  );
+}
+
+function MemoEditor({
+  memo,
+  text,
+  setText,
+  memos,
+  handleUpdateButtonClick,
+  handleDeleteButtonClick,
+}) {
+  return (
+    <section>
+      <MemoTextArea
+        memo={memo}
+        text={text}
+        setText={setText}
+        memos={memos}
+        handleUpdateButtonClick={handleUpdateButtonClick}
+        handleDeleteButtonClick={handleDeleteButtonClick}
+      />
+    </section>
+  );
+}
+
+let nextId = 3;
+
+const MEMOS = [
+  {
+    id: 0,
+    title: "メモ1",
+    content: "メモ1\nメモ1の内容\nメモ1の内容\nメモ1の内容",
+  },
+  {
+    id: 1,
+    title: "メモ2",
+    content: "メモ2\nメモ2の内容\nメモ2の内容\nメモ2の内容",
+  },
+  {
+    id: 2,
+    title: "メモ3",
+    content: "メモ3\nメモ3の内容\nメモ3の内容\nメモ3の内容",
+  },
+];
+
+export default function MemoApps() {
+  const [memos, setMemos] = useState(MEMOS);
+  const [editingMemo, setEditingMemo] = useState(MEMOS[0]);
+  const [text, setText] = useState(editingMemo.content);
+
+  function handleMemoClick(memo) {
+    setEditingMemo(memo);
+    setText(memo.content);
+  }
+
+  function handleAddButtonClick(memos) {
+    const nextMemo = { id: nextId++, title: "新規メモ", content: "" };
+    setMemos([...memos, nextMemo]);
+    handleMemoClick(nextMemo);
+  }
+
+  function handleUpdateButtonClick(memo, text) {
+    const updatedMemo = { ...memo, title: text.split("\n")[0], content: text };
+    setMemos(
+      memos.map((m) => {
+        if (m.id === memo.id) {
+          return updatedMemo;
+        } else {
+          return m;
+        }
+      }),
+    );
+    setEditingMemo(updatedMemo);
+    setText(updatedMemo.content);
+  }
+
+  function handleDeleteButtonClick(memos, memo) {
+    setMemos(memos.filter((m) => m.id !== memo.id));
+  }
+
+  return (
+    <section>
+      <MemoIndex
+        memos={memos}
+        handleMemoClick={handleMemoClick}
+        handleAddButtonClick={handleAddButtonClick}
+      />
+      <MemoEditor
+        memo={editingMemo}
+        text={text}
+        setText={setText}
+        memos={memos}
+        handleUpdateButtonClick={handleUpdateButtonClick}
+        handleDeleteButtonClick={handleDeleteButtonClick}
+      />
+    </section>
+  );
+}
